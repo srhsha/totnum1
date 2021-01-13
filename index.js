7820f413933c0e7192fa1c1ed44b2f9ec5d3f9d0
srhsha <programmer00@naver.com>
2021-01-14 00:14:44 +0900

    html commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, (req, res)=>{
+    console.log(port+' port listen');
+});
